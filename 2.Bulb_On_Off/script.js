09fe8053de4860cb4f00d7e319deeda36bd71314
Immu <shaikimam37302@gmail.com>
2025-05-16 10:31:15 +0530

    JS Projects

---
+++ 2.Bulb_On_Off/script.js
@@ -0,0 +1,6 @@
+function bulbON(){
+    document.getElementById("image").src = "./Assets/bulb-on.png";
+}
+function bulbOFF(){
+    document.getElementById("image").src = "./Assets/bulb-off.png";
+}
